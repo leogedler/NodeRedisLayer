e70732fe9e627ec9f4d658020bcef55af730dbc3
Leonardo Gedler <leonardo.gedler@macys.com>
2018-12-16 12:54:32 -0300

    Travis config

---
+++ config/ci.js
@@ -0,0 +1,9 @@
+module.exports = {
+    googleClientID:
+      '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+    googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+    mongoURI: 'mongodb://localhost:27017/blog_ci',
+    redisURI: 'redis://127.0.0.1:6379',
+    cookieKey: '123123123'
+  };
+  
